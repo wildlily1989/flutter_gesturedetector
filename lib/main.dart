a588ae3995395dc00e4c7f953333ea578a872aa5
lily <614720105@qq.com>
2020-04-28 18:43:19 +0800

    time:2020.04.28 author:lily content:手势识别。滑动删除

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,8 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
 //      home: new MyButton(title: 'mybutton'),
-    home: new InkWellButton(),
+//    home: new InkWellButton(),
+    home: Slide(),
     );
   }
 }
@@ -79,4 +80,35 @@ class InkWellButton extends StatelessWidget{
     );
     throw UnimplementedError();
   }
+}
+
+class Slide extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   final items =new List<String>.generate(20, (i) => "Item ${i + 1}");
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Slide'),
+      ),
+      body: new ListView.builder(
+        itemCount: items.length,
+          itemBuilder: (context,index){
+          final item = items[index];
+
+          return new Dismissible(
+              key: new Key(item),
+              onDismissed: (direction){
+                items.removeAt(index);
+                Scaffold.of(context).showSnackBar(
+                  new SnackBar(content: new Text("$item dismissed}"))
+                );
+              },
+          background: new Container(color: Colors.red,),
+            child: new ListTile(title: new Text('$items'),),
+          );
+          }),
+    );
+    throw UnimplementedError();
+  }
+
 }
